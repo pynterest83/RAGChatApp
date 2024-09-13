a9257b0d31df9af89d9790c75727f6e6113e866e
Chu Huy Quang <quangdtm2004@gmail.com>
2024-09-13 11:25:31 +0700

    baseline

--- backend/main.py
+++ backend/main.py
@@ -1,35 +1,70 @@
-#!/usr/bin/env python
-from dotenv import load_dotenv
-import os
-# Load environment variables from .env file
-load_dotenv()
-
-from fastapi import FastAPI, File, UploadFile, HTTPException, Depends
-from fastapi.responses import RedirectResponse
-from fastapi.middleware.cors import CORSMiddleware
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_core.runnables import RunnableParallel, RunnablePassthrough
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
-from langchain_community.chat_models import ChatCohere
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_community.vectorstores import Qdrant
+from langchain_cohere import CohereEmbeddings, ChatCohere
+from langchain_qdrant import QdrantVectorStore
 from qdrant_client import QdrantClient
-from langchain_cohere import CohereEmbeddings
-import os
-from langchain_core.pydantic_v1 import BaseModel
-from typing import List
-import shutil
-from fastapi import Query, Response, Cookie  # Import Query for optional query parameters
-from qdrant_client.http import models
-import secrets
-# Load environment variables
-COHERE_API_KEY = "p2Jch3wcKWOD5I2QpMaamiSCkVcgjDEzSsyLyHf4"
-# Global variable to track if the document has been uploaded and processed
-document_processed = False
-# Initialize embeddings and Qdrant client outside of the route to reuse
-embeddings = CohereEmbeddings(model="embed-english-light-v3.0", cohere_api_key=COHERE_API_KEY)
-client = QdrantClient(url="http://localhost:6333")
-collection_name = "my_documents"
-qdrant_client = Qdrant(client, collection_name, embeddings)
+from qdrant_client.http.models import Distance, VectorParams
+from uuid import uuid4
+from langchain_core.documents import Document
+from langchain_community.document_loaders import PyPDFLoader, TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.output_parsers import StrOutputParser
+from langchain.chains import ConversationalRetrievalChain
+import torch
+from transformers import BitsAndBytesConfig
+from transformers import AutoTokenizer , AutoModelForCausalLM ,pipeline
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_huggingface.llms import HuggingFacePipeline
+from langchain.memory import ConversationBufferMemory
+from langchain_community . chat_message_histories import ChatMessageHistory
+from langchain_core.runnables import RunnablePassthrough
+from langchain import hub
 
+embeddings = CohereEmbeddings(model="embed-english-light-v3.0", cohere_api_key="p2Jch3wcKWOD5I2QpMaamiSCkVcgjDEzSsyLyHf4")
+
+client = QdrantClient(url="http://localhost:6333")
+
+client.create_collection(
+    collection_name="client_documents",
+    vectors_config=VectorParams(size=384, distance=Distance.COSINE),
+)
+
+vector_store = QdrantVectorStore(
+    client=client,
+    collection_name="client_documents",
+    embedding=embeddings,
+)
+
+retriever = vector_store.as_retriever()
+
+Loader = PyPDFLoader
+FILE_PATH = "D:/Code/RAGChatApp/test/2404.05961v2.pdf"
+loader = Loader(FILE_PATH)
+documents = loader.load()   # Load the document
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+docs = text_splitter.split_documents(documents)
+print("Number of documents: ", len(docs))
+print(docs[0])
+
+result =retriever.invoke("What is the main idea of the document?")
+
+print(result)
+
+llm = ChatCohere(model="command-r-plus")
+
+prompt = hub. pull ("rlm/rag-prompt")
+
+def format_docs ( docs ) :
+    return "\n\n". join (doc . page_content for doc in docs )
+
+rag_chain = (
+    {" context ": retriever | format_docs , " question ": RunnablePassthrough
+    () }
+    | prompt
+    | llm
+    | StrOutputParser ()
+)
+
+USER_QUESTION = " YOLOv10 là gì?"
+output = rag_chain . invoke ( USER_QUESTION )
+answer = output . split ('Answer :') [1]. strip ()
+print (answer)
